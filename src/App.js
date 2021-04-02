18904c657abd6cbf4cb390943c967aae3def070d
khoinguyenkc <khoinguyenkc@gmail.com>
2021-04-01 23:46:14 -0700

    commit tree 6d0fcffd495139b60f975dbd2aef674b3ce2efff

--- src/App.js
+++ src/App.js
@@ -1,1 +1,27 @@
 // create your App component here
+import React, { Component} from 'react';
+
+export default class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      list: []
+    }
+  }
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json').then( resp => resp.json() ).then( json => {
+      this.setState({
+        list: json
+      })
+    })
+  }
+
+  render() {
+    return (
+        <h1>hi ca nha yeu cua kem</h1>
+    )
+  }
+
+
+}
